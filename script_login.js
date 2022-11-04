e8748a738c7cf6084de68e1d5bd5fb0c2747fcde
lucaramiandrasoa <lucaramiandrasoa@gmail.com>
2022-11-04 09:19:24 +0100

    register et login ok

--- script_login.js
+++ script_login.js
@@ -1,6 +1,6 @@
 const signup = document.querySelector('.t-signup');
 const login = document.querySelector('.t-login');
-const addClass = document.querySelector('.side');
+const addClass = document.querySelector('.site');
 
 signup.addEventListener('click', function(){
     addClass.className = 'site signup-show';

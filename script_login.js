f7f8315b9d6656f46f023f7f2df113f26a42a4e4
lucaramiandrasoa <lucaramiandrasoa@gmail.com>
2022-11-04 07:46:29 +0100

    CSS en place

---
+++ script_login.js
@@ -0,0 +1,11 @@
+const signup = document.querySelector('.t-signup');
+const login = document.querySelector('.t-login');
+const addClass = document.querySelector('.side');
+
+signup.addEventListener('click', function(){
+    addClass.className = 'site signup-show';
+})
+
+login.addEventListener('click', function(){
+    addClass.className = 'site login-show';
+})
